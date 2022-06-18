f2fef80dc17f40701c45c55527bcea34bbe6918f
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-06-17 21:34:29 -0300

    update

---
+++ src/main/java/com/craivet/ProyectoGradle.java
@@ -0,0 +1,7 @@
+package com.craivet;
+
+public class ProyectoGradle {
+	public static void main(String[] args) {
+		System.out.println("Rulo quemado");
+	}
+}
---
+++ src/test/java/com/craivet/ProyectoGradleTest.java
@@ -0,0 +1,12 @@
+package com.craivet;
+
+import org.junit.Test;
+
+public class ProyectoGradleTest {
+	
+	@Test
+	public void verifyNoExceptionThrown() {
+		ProyectoGradle.main(new String[]{});
+	}
+
+}
